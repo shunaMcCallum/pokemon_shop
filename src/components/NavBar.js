a1f1c10a3fb32f901f459a0bd0623479e8cfd75b
shunaMcCallum <shuna.mccallum@gmail.com>
2022-07-06 21:46:10 +0100

    routes added and all currently working within the app structure

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const NavBar = ({ handleClick }) => {
+const NavBar = () => {
 
 
     return (
@@ -9,7 +9,7 @@ const NavBar = ({ handleClick }) => {
             <div>
                 <ul>
                     <li>
-                        <Link to="/" onClick={handleClick}>Home</Link>
+                        <Link to="/">Home</Link>
                     </li>
 
                     <li>
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ListItem from './ListItem';
 import './ListItem.css'
+import { Link } from "react-router-dom";
 
 const PokemonList = ({pokemonList, onPokemonClick}) => {
 
     const pokemonNodes = pokemonList.map((pokemon, index) => {
-        return <ListItem key={index} pokemon={pokemon} onPokemonClick={onPokemonClick} />
+        return (
+            <Link to={`/pokemon/${pokemon.id}`}>
+                <ListItem key={index} pokemon={pokemon} onPokemonClick={onPokemonClick} />
+            </Link>
+        )
     });
 
     return (
